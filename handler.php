61bb313fcba56fb2f28c220ddadb4311a5f33bb8
Valentin <vavalik999@gmail.com>
2019-09-05 01:53:27 +0300

    SUCCESS CONNCTION TO HEROKU

--- handler.php
+++ handler.php
@@ -8,19 +8,19 @@
 
 	$record = $data['record'];
 
-	var_dump(PDO::getAvailableDrivers());
-	echo "<br />";
-	//$record можно заночить в бд
-	$driver = 'pgsql';				//тип бд
-	$host = 'ec2-54-246-121-32.eu-west-1.compute.amazonaws.com';				//то что в url строке 
+	//var_dump(PDO::getAvailableDrivers());
+	//echo "<br />";
+
+	//ЭТО РАБОТАЕТ НА HEROKU
+	$driver = 'pgsql';				
+	$host = 'ec2-54-246-121-32.eu-west-1.compute.amazonaws.com';				
 	$bd_name = 'dfgngu63o3bvv0';
 	$user = "siunpcbvdiimna";
 	$charset = 'utf8';
-	$pass = '5aceb95a0d56b72ac10c3ed0e3fb0465374f7a59d71c2727e47e67ac9853c7a1';						//пароль от бд
+	$pass = '5aceb95a0d56b72ac10c3ed0e3fb0465374f7a59d71c2727e47e67ac9853c7a1';
 	$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
 
-//echo "$driver:host=$host;bdname=$bdname;charset=$charset";
-//phpinfo();
+
 
 	try {  
 		$DBH = new PDO("$driver:host=$host;dbname=$bd_name",$user,$pass);
