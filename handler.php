2474f467621b884a68dc533532e7eb0de0b52688
Valentin <vavalik999@gmail.com>
2019-09-05 01:42:22 +0300

    11

--- handler.php
+++ handler.php
@@ -9,10 +9,12 @@
 	$record = $data['record'];
 
 	var_dump(PDO::getAvailableDrivers());
+	echo "<br />";
 	//$record можно заночить в бд
 	$driver = 'pgsql';				//тип бд
 	$host = 'ec2-54-246-121-32.eu-west-1.compute.amazonaws.com';				//то что в url строке 
-	$bd_user = 'dfgngu63o3bvv0';				//логин от бд
+	$bd_name = 'dfgngu63o3bvv0';
+	$user = "siunpcbvdiimna";
 	$charset = 'utf8';
 	$pass = '5aceb95a0d56b72ac10c3ed0e3fb0465374f7a59d71c2727e47e67ac9853c7a1';						//пароль от бд
 	$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
@@ -21,7 +23,7 @@
 //phpinfo();
 
 	try {  
-		$DBH = new PDO("$driver:host=$host;dbname=$bd_user",$bd_user,$pass);
+		$DBH = new PDO("$driver:host=$host;dbname=$bd_name",$user,$pass);
 		echo "соединение прошло усешно!\nПрекрасно! Поздравляю!";
 	}  
 	catch(PDOException $e) {  
