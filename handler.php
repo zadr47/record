b3933a52b5ec018bd333a33acc4d1bf628579e1c
Valentin <vavalik999@gmail.com>
2019-09-04 23:56:41 +0300

    4

--- handler.php
+++ handler.php
@@ -8,16 +8,16 @@
 
 	$record = $data['record'];
 
+	var_dump(PDO::getAvailableDrivers());
 	//$record можно заночить в бд
 	$driver = 'mysql';				//тип бд
 	$host = 'ec2-54-246-121-32.eu-west-1.compute.amazonaws.com';				//то что в url строке 
-	$bd_user = 'siunpcbvdiimna';				//логин от бд
+	$bd_user = 'dfgngu63o3bvv0';				//логин от бд
 	$charset = 'utf8';
 	$pass = '5aceb95a0d56b72ac10c3ed0e3fb0465374f7a59d71c2727e47e67ac9853c7a1';						//пароль от бд
 	$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
 
 //echo "$driver:host=$host;bdname=$bdname;charset=$charset";
-//var_dump(PDO::getAvailableDrivers());
 //phpinfo();
 
 	try {  
