b0db8a089c531c09e7cf6ec0aab9b6973893554a
Valentin <vavalik999@gmail.com>
2019-09-07 04:54:28 +0300

    47

--- function.php
+++ function.php
@@ -7,9 +7,9 @@
 			$result_query = $conn->query($sql);	
 
 		}catch(PDOException $e) {  
-
+			
 			//$sql = "CREATE TABLE record.records ( id INT(11) NOT NULL , record VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ) ENGINE = MyISAM CHARSET=utf8 COLLATE utf8_general_ci;";
-			$sql = "CREATE TABLE record.records ( id INT NOT NULL , record VARCHAR NOT NULL );";
+			$sql = "CREATE TABLE records ( id INT , record VARCHAR(255) );";
 			$conn->query($sql);		  
 		}
 		$conn = NULL;
