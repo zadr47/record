e3dc5d2331253b730fd781092db9e2705e194e71
Valentin <vavalik999@gmail.com>
2019-09-07 03:20:41 +0300

    40

--- function.php
+++ function.php
@@ -1,15 +1,16 @@
 <?php
 	function create_table_records(){
-		require_once($_SERVER['DOCUMENT_ROOT'].'/connection.php');
-		//$sql = "SHOW TABLES LIKE  'records';";
-		$sql = "SHOW TABLES LIKE `records`;";
-		$conn = conn();
-		$result_query = $conn->query($sql);
-		$num = $result_query->rowCount();
-		if($num==0){			
+
+		try{
+			$sql = "SELECT * FROM records;";
+			$conn = conn();
+			$result_query = $conn->query($sql);	
+
+		}catch(PDOException $e) {  
+
 			$sql = "CREATE TABLE `record`.`records` ( `id` INT(11) UNSIGNED NOT NULL , `record` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL , UNIQUE `id` (`id`)) ENGINE = MyISAM;";
 			require_once($_SERVER['DOCUMENT_ROOT'].'/connection.php');
-			$conn->query($sql);
+			$conn->query($sql);		  
 		}
 		$conn = NULL;
 	}
